0c76939a8f6a25aba16587685e6aaae470633815
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-06-01 14:18:54 +0900

    example

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+"""例
+class Card(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    liked = models.IntegerField(default=0)
+    card_date = models.DateTimeField('date published', default=timezone.now)
+    card_tag = models.TextField()
+    card_caregory = models.TextField()
+    user = models.ForeignKey(User,related_name='cards',on_delete=models.CASCADE,default="0")
+"""
