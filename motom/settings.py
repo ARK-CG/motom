3cdf73e48c2bda84a0d412ca7418e741191ddec4
ErgoFriend <ergoproxy2236@yahoo.co.jp>
2018-07-07 10:02:39 +0900

    asd

--- motom/settings.py
+++ motom/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
